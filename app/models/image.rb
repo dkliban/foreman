04f824ea0717a7ebc8abb20744db816887744bda
Dennis Kliban <dkliban@redhat.com>
2014-09-08 18:51:09 +0000

    UUID for ImageFactory images
    When adding an image to Image Factory compute resource the UUID is
    generating by POSTing to the compute resource URL +
    '/imagefactory/base_images'.  The current template just requests
    a MockOS image.

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,7 +1,7 @@
 class Image < ActiveRecord::Base
   include Authorizable
   
-  before_save :check_compute_resource
+  before_validation :check_compute_resource
 
   audited :allow_mass_assignment => true
 
@@ -22,6 +22,18 @@ class Image < ActiveRecord::Base
   scoped_search :on => :user_data, :complete_value => {:true => true, :false => false}
 
   def check_compute_resource
-    response = RestClient.get 'http://www.google.com'
+    # Only do this for the Image Factory compute resource images
+    if compute_resource.type == "Foreman::Model::Imgfac"
+      begin
+        url = compute_resource.url + "/imagefactory/base_images"
+        body = '{"base_image": {"template":"<template><name>buildbase_image</name><os><name>MockOS_A</name><version>1</version><arch>x86_64</arch><install type=\'url\'><url>http://download.devel.redhat.com/released/F-17/GOLD/Fedora/x86_64/os/</url></install><rootpw>password</rootpw></os><description>Tests building a base_image</description></template>"}}'
+        debugger
+        response = JSON.load(RestClient.post url, body, :content_type => :json, :accept => :json)
+        self.uuid = response['base_image']['id'] 
+      rescue => e
+        errors.add(:base, e.message)
+        return false 
+      end
+    end
   end
 end
